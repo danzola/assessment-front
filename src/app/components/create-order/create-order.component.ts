241304414fb3c76a77576011ac0367676bd9ee72
Diego Anzola <diego_anzola@yahoo.es>
2025-03-29 17:32:56 -0500

    implement create order functionality with product selection and order submission; enhance view products component

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ViewProductsComponent } from '../view-products/view-products.component';
 
 @Component({
   selector: 'app-create-order',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ViewProductsComponent
+  ],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.scss'
 })
-export class CreateOrderComponent {
+export class CreateOrderComponent implements AfterViewInit {
+  @ViewChild(ViewProductsComponent) productsList!: ViewProductsComponent;
+  createOrder: boolean = true;
+
+  order = {
+    customerId: 0,
+    productId: 0,
+    quantity: 0
+  };
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.productsList.productSelected.subscribe((product) => {
+      this.order.productId = product.id;
+      this.setStateSearchProducts(false)
+    });
+  }
+
+  searchProduct(): void {
+    this.setStateSearchProducts(true);
+  }
+
+  setStateSearchProducts(state: boolean): void {
+    this.createOrder = !state;
+    this.productsList.searchVisible = state;
+  }
+
+  submitOrder() {
+    console.log('Order Submitted:', this.order);    
+  }
 
 }
--- src/app/components/view-products/view-products.component.ts
+++ src/app/components/view-products/view-products.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-view-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-products.component.html',
   styleUrl: './view-products.component.scss'
 })
-export class ViewProductsComponent {
+export class ViewProductsComponent implements OnInit {  
+  @Output() productSelected = new EventEmitter<any>();
+  products: any[] = [];
+  searchVisible: boolean = false;
 
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll().subscribe((data: any) => {
+      this.products = data;
+    });
+  }
+
+  selectProduct(product: any): void {
+    this.productSelected.emit(product);
+  }
 }
